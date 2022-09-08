fe77070cf40a29c6aa30b6e2c9dcc10c9058f6d4
Tshimologo King Mabena <Tshimologokingmabena@gmail.com>
2022-09-08 20:58:06 +0200

    "update"

--- wyze_up/docFile/contollers/userController.js
+++ wyze_up/docFile/contollers/userController.js
@@ -7,40 +7,37 @@ require("dotenv").config();
 
 //ADD SUBSCRIBTION
 async function addSubscription(req, res) {
-    if (req.users.userType == "user") {
-        const {id, status, userName, userEmail} = req.body;
-        try{
-            connection.query(`INSERT INTO subscriptions(id, status, userName, userEmail) VALUES
-            ("${id}", "${status}", "${userName}", "${userEmail}")`,
-            (err, result) => {
-                if (err) throw err;
-                res.send(result);
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(404).send(error);
-        }
-    } else {
-        res.send("Not a user")
+   
+    const {id, status, userName, userEmail} = req.body;
+    try{
+        connection.query(`INSERT INTO subscriptions(id, status, userName, userEmail) VALUES
+        ("${id}", "${status}", "${userName}", "${userEmail}")`,
+        (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error);
     }
 }
 
 //DELETE SUBSCRIPTION
 async function cancelSubscription(req, res) {
-    if (req.users.userType === "user") {
-        try {
-          connection.query(
-            `DELETE FROM subscriptions WHERE id=${req.params.id}`,
-            (err, result) => {
-              if (err) throw err;
-              res.send(result);
-            }
-          );
-        } catch (error) {
-          console.log(error);
-          res.status(400).send(error);
+    
+    try {
+        connection.query(
+        `DELETE FROM subscriptions WHERE id=${req.params.id}`,
+        (err, result) => {
+            if (err) throw err;
+            res.send(result);
         }
+        );
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error);
     }
+
 }
 
 
